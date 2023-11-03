a2bbb7189ac5e12cb9641ce7adc69a0287f9d817
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2023-11-03 20:42:57 +0700

    xong chức năng login, createdate, message data, băm update, trả ra hết database quản lý user, findAll, findOne, deleteuser gần xong quản lý vận đơn, chuẩn bị làm phân quyền xong database xong shipment, đang làm product xong tính năng click nhận đơn xong tính năng update workstore CORS 3000

---
+++ src/main/java/com/supership/ship/security/SecurityConfig.java
@@ -0,0 +1,12 @@
+//package com.supership.ship.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    // Cấu hình xác thực và phân quyền ở đây
+//}
